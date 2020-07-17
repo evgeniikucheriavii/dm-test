ae9207f6fe90ab72571e2ffba247e450870d9172
Evgenii Kucheriavyi <e.kucheriavii@gmail.com>
2020-07-17 12:18:50 +0300

    Added clients list

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-clients',
-  templateUrl: './clients.component.html',
-  styleUrls: ['./clients.component.css']
+	selector: 'app-clients',
+	templateUrl: './clients.component.html',
+	styleUrls: ['./clients.component.css']
 })
-export class ClientsComponent implements OnInit {
+export class ClientsComponent implements OnInit 
+{
 
-  constructor() { }
+	clients = [];
+	tabs = [];
+	currentClient:string;
+	currentTab:number;
 
-  ngOnInit(): void {
-  }
+	constructor() { }
 
+	ngOnInit(): void 
+	{
+		this.currentClient = "Колесов А. В.";
+		
+		this.clients = [
+			new Client("Иванов Иван", 0, 15),
+			new Client("Сидоров Сергей", 0, 10),
+			new Client("Колесов А. В.", 0, 12),
+			new Client("Петрова Светлана", 0, 5),
+			new Client("Баярова Алина", 0, 22),
+			new Client("Аппарат Экзарта", 0, 31),
+			new Client("Платформа Галилео", 0, 3),
+			new Client("Петрова Светлана", 0, 18),
+			new Client("Баярова Алина", 0, 22),
+			new Client("Петрова Светлана", 0, 33),
+			new Client("Аппарат Экзарта", 0, 15),
+			new Client("Платформа Галилео", 0, 10),
+			new Client("Петрова Светлана", 0, 12),
+			new Client("Аппарат Экзарта", 0, 5)
+		];
+	}
+
+}
+
+
+class Client
+{
+	fio:string;
+	sale:boolean = false;
+	rfm:number;
+	ltv:number;
+
+	constructor(fio:string, rfm:number, ltv:number)
+	{
+		this.fio = fio;
+		this.rfm = rfm;
+		this.ltv = ltv;
+	}
 }
