f4155543b906cc5773c6e81a2befd806f5e709ac
Evgenii Kucheriavyi <e.kucheriavii@gmail.com>
2020-07-18 14:35:33 +0300

    Added recomendations page main sections

--- src/app/recomendations-engine/recomendations-engine.component.ts
+++ src/app/recomendations-engine/recomendations-engine.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Tab } from '../tab';
 
 @Component({
-  selector: 'app-recomendations-engine',
-  templateUrl: './recomendations-engine.component.html',
-  styleUrls: ['./recomendations-engine.component.css']
+	selector: 'app-recomendations-engine',
+	templateUrl: './recomendations-engine.component.html',
+	styleUrls: ['./recomendations-engine.component.css']
 })
-export class RecomendationsEngineComponent implements OnInit {
+export class RecomendationsEngineComponent implements OnInit 
+{
+	tabs = [];
+	currentTab:number;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void 
+	{
+		this.tabs = [
+			new Tab("utilization-rules", "Правила экстренной утилизации"),
+			new Tab("actions", "Action Center")
+		];
 
+		this.currentTab = 0;
+
+		this.tabs[this.currentTab].Activate();
+	}
+
+	public SwitchTab(index:number)
+	{
+		if(index <= this.tabs.length)
+		{
+			this.currentTab = index;
+
+			for(let i = 0; i < this.tabs.length; i++)
+			{
+				this.tabs[i].Deactivate();
+			}
+
+			this.tabs[this.currentTab].Activate();
+		}
+	}
 }
