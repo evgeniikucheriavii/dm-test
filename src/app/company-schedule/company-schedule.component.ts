246007da5ca504b3f6b0e41a4e141688fb0844ae
Evgenii Kucheriavyi <e.kucheriavii@gmail.com>
2020-08-03 10:12:39 +0300

    Added left section

--- src/app/company-schedule/company-schedule.component.ts
+++ src/app/company-schedule/company-schedule.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-company-schedule',
-  templateUrl: './company-schedule.component.html',
-  styleUrls: ['./company-schedule.component.css']
+	selector: 'app-company-schedule',
+	templateUrl: './company-schedule.component.html',
+	styleUrls: ['./company-schedule.component.css']
 })
-export class CompanyScheduleComponent implements OnInit {
+export class CompanyScheduleComponent implements OnInit 
+{
 
-  constructor() { }
+	
 
-  ngOnInit(): void {
-  }
+	constructor() { }
+
+	ngOnInit(): void 
+	{
+	}
 
 }
